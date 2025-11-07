2f218bde4d9165b8ab012d4523c0e3d368345506
nickolay <nickolay8@gmail.com>
2025-11-07 12:59:26 +0300

    progress

---
+++ src/best_time_to_buy_and_sell_stock_ii.ts
@@ -0,0 +1,30 @@
+function maxProfit(prices: number[]): number {
+    let total_profit = 0
+
+    let start_price = prices[0]
+    let profit = 0
+    let max_profit = 0
+
+    for (let i = 1; i < prices.length; i++) {
+        const price = prices[i]
+        const delta = price - prices[i - 1]
+
+        if (profit + delta < max_profit) {
+            total_profit += max_profit
+            max_profit = 0
+            profit = 0
+            start_price = price
+            continue
+        }
+
+        profit += delta
+
+        if (profit > max_profit) {
+            max_profit = profit
+        }
+    }
+
+    total_profit += max_profit
+
+    return total_profit
+};
