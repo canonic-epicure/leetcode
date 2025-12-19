75b8b3184aad5142e2dafc32ae6d322c4fd0009a
nickolay <nickolay8@gmail.com>
2025-12-19 10:35:55 +0300

    progress

---
+++ src/binary_tree_bfs/average_of_levels_in_binary_tree.ts
@@ -0,0 +1,30 @@
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.left = (left===undefined ? null : left)
+        this.right = (right===undefined ? null : right)
+    }
+}
+
+function averageOfLevels(root: TreeNode | null): number[] {
+    const res: number[] = []
+
+    let level: TreeNode[] = [ root ]
+
+    while (level.length) {
+        res.push(
+            level.reduce((acc, el) => acc + el.val, 0) / level.length
+        )
+
+        level = level.reduce((acc, el) => {
+            if (el.left) acc.push(el.left)
+            if (el.right) acc.push(el.right)
+            return acc
+        }, [])
+    }
+
+    return res
+};
