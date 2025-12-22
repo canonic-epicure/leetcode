2c735beece714794937fc32cb2d9217f118dc323
nickolay <nickolay8@gmail.com>
2025-12-22 15:17:08 +0300

    progress

---
+++ src/graph_general/course_schedule.ts
@@ -0,0 +1,50 @@
+function canFinish(numCourses: number, prerequisites: [number, number][]): boolean {
+    const graph: Map<number, Set<number>> = new Map()
+
+    prerequisites.forEach(([a, b]) => {
+        let from = graph.get(b)
+
+        if (!from) {
+            from = new Set<number>()
+
+            graph.set(b, from)
+        }
+
+        from.add(a)
+    })
+
+    const visited: Set<number> = new Set()
+
+    const dfs = (from: number, vis: Set<number>): boolean => {
+        if (visited.has(from)) return true
+
+        if (vis.has(from)) {
+            return false
+        }
+
+        vis.add(from)
+
+        const tos = graph.get(from)
+
+        if (tos) {
+            for (const to of tos) {
+                if (dfs(to, vis) === false) return false
+            }
+        }
+        visited.add(from)
+
+        return true
+    }
+
+    for (let i = 0; i < numCourses; i++) {
+        if (visited.has(i)) continue
+
+        if (dfs(i, new Set()) === false) return false
+    }
+
+    return true
+};
+
+console.log(
+    canFinish(4, [[1,0],[2,0],[3,1],[3,2]])
+)
